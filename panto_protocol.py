041d56105722a08ac8e8eff509a238cb41cb64c4
martintaraz <martintaraz@local>
2024-04-14 12:33:42 +0200

    more testcases
    bumped version of the panto firmware
    
    implemented protocol pickup and bootloop detection for the panto protocol

--- panto_protocol.py
+++ panto_protocol.py
@@ -3,6 +3,7 @@ import logging
 import time
 from enum import Enum
 
+import binascii
 from serial import Serial
 
 logging.basicConfig(level=logging.DEBUG)
@@ -30,36 +31,43 @@ class PantoMessageType(Enum):
 
 @dataclasses.dataclass()
 class PositionData:
-    x1: int
-    y1: int
-    x2: int
-    y2: int
-    r1: int
-    r2: int
-    g_x1: int
-    g_y1: int
-    g_x2: int
-    g_y2: int
+    x1: int | float
+    y1: int | float
+    x2: int | float
+    y2: int | float
+    r1: int | float
+    r2: int | float
+    g_x1: int | float
+    g_y1: int | float
+    g_x2: int | float
+    g_y2: int | float
 
 
 class PantoProtocol:
     def __init__(self, serial: Serial, timeout=10):
+        logging.debug(f"Initializing PantoProtocol with serial {serial} and timeout {timeout}")
         self.con: Serial = serial
         self.timeout = timeout
-        self.messages_received = []
+        self.messages_received: list[bytes] = []
+        self.messages_sent: list[bytes] = []
+        self.messages_to_sent: list[bytes] = []
 
     def handle_sync(self, body):
-        self.con.write(bytes.fromhex("445080000000"))
+        self.messages_to_sent.append(bytes.fromhex("445080000000"))
 
     def handle_heartbeat(self, body):
-        self.con.write(bytes.fromhex("445081000000"))
+        self.messages_to_sent.append(bytes.fromhex("445081000000"))
 
     def handle_debug(self, body):
         logging.info("received debug message:")
         logging.info(body)
 
+    def handle_transition_ended(self, body):
+        return
+
     def verify_header(self, header: bytes):
-        return True
+        if header != b'DP':
+            raise ValueError('Invalid Panto protocol header', binascii.hexlify(header))
 
     def verify_position(self, position: PositionData):
         return True
@@ -78,27 +86,115 @@ class PantoProtocol:
 
         self.verify_position(PositionData(x_1, y_1, x_2, y_2, r_1, r_2, g_x1, g_y1, g_x2, g_y2))
 
+    def before_message(self):
+        pass
+
     def run(self):
-        start_time = time.time()
-        while start_time + self.timeout > time.time():
-            header = self.con.read(size=2)
-            self.verify_header(header)
+        try:
+            # consume single bytes until messages align
+            """
+            somtimes flashing the esp fails and the esp is stuck in a boot loop
+            https://esp32.com/viewtopic.php?t=23203
+            The esp is then stuck in a boot loop showing these messages here over and over again
+            
+            1162 mmu set 00010000, pos 00010000
+            1162 mmu set 00020000, pos 00020000
+            1162 mmu set 00030000, pos 00030000
+            1162 mmu set 00040000, pos 00040000
+            """
 
-            msg_type = self.con.read(size=1)
+            good_sequence = "DP".encode("ascii")
+            good_sequence_idx = 0
+            bad_sequence = "1162 mmu set".encode("ascii")
+            bad_sequence_idx = 0
 
-            package_id = self.con.read(size=1)
+            num_bytes_lost = 0
 
-            message_size = self.con.read(size=2)
+            while num_bytes_lost < 100:
+                _byte = self.con.read(1)
+                if _byte == good_sequence[good_sequence_idx].to_bytes():
+                    good_sequence_idx += 1
+                else:
+                    good_sequence_idx = 0
 
-            body = self.con.read(size=int.from_bytes(message_size, byteorder="big"))
+                if _byte == bad_sequence[bad_sequence_idx].to_bytes():
+                    bad_sequence_idx += 1
+                else:
+                    bad_sequence_idx = 0
 
-            if msg_type == PantoMessageType.SYNC:
-                self.handle_sync(body)
-            elif msg_type == PantoMessageType.HEARTBEAT:
-                self.handle_heartbeat(body)
-            elif msg_type == PantoMessageType.POSITION:
-                self.handle_position(body)
-            elif msg_type == PantoMessageType.DEBUG_LOG:
-                self.handle_debug(body)
+                logging.info("Received byte: %s", _byte)
 
-            self.messages_received.append(header + msg_type + package_id + message_size + body)
+                if good_sequence_idx == len(good_sequence):
+                    logging.info("Seeking found good sequence... consuming message than switching to protocol")
+                    msg_type = self.con.read(size=1)
+                    package_id = self.con.read(size=1)
+                    message_size = self.con.read(size=2)
+                    body = self.con.read(size=int.from_bytes(message_size, byteorder="big"))
+                    logging.info("consumed message... switching to protocol")
+                    logging.info(f"msgtype: {binascii.hexlify(msg_type)}")
+                    logging.info(f"package_id: {binascii.hexlify(package_id)}")
+                    logging.info(f"message_size: {binascii.hexlify(message_size)}")
+                    logging.info(f"body: {binascii.hexlify(body)}")
+                    break
+
+                if bad_sequence_idx == len(bad_sequence):
+                    raise ValueError("Bad sequence found - Stuck in a  boot loop")
+            else:
+                raise ValueError("No start sequence matched...")
+
+            start_time = time.time()
+            while (start_time + self.timeout) > time.time():
+                self.before_message()
+                for message in self.messages_to_sent:
+                    self.con.write(message)
+                    self.messages_sent.append(message)
+                self.messages_to_sent = []
+
+                header = self.con.read(size=2)
+                self.verify_header(header)
+
+                msg_type = self.con.read(size=1)
+                msg_type_as_int = int.from_bytes(msg_type, byteorder='big', signed=False)
+
+                package_id = self.con.read(size=1)
+
+                message_size = self.con.read(size=2)
+
+                body = self.con.read(size=int.from_bytes(message_size, byteorder="big"))
+
+                if msg_type_as_int == PantoMessageType.SYNC.value:
+                    self.handle_sync(body)
+                elif msg_type_as_int == PantoMessageType.HEARTBEAT.value:
+                    self.handle_heartbeat(body)
+                elif msg_type_as_int == PantoMessageType.POSITION.value:
+                    self.handle_position(body)
+                elif msg_type_as_int == PantoMessageType.DEBUG_LOG.value:
+                    self.handle_debug(body)
+                elif msg_type_as_int == PantoMessageType.PACKET_ACK.value:
+                    continue
+                elif msg_type_as_int == PantoMessageType.TRANSITION_ENDED.value:
+                    self.handle_transition_ended(body)
+                elif msg_type_as_int == PantoMessageType.INVALID_DATA.value:
+                    logging.debug(f"Received INVALID DATA packet")
+                    if len(self.messages_sent) > 0:
+                        logging.debug("last message sent")
+                        logging.debug(binascii.hexlify(self.messages_sent[-1]))
+                    else:
+                        logging.debug(f"No messages sent before")
+                else:
+                    logging.debug(f"Unknown message type {msg_type}")
+                    logging.debug(f"Message {binascii.hexlify(header + msg_type + package_id + message_size + body)}")
+                self.messages_received.append(header + msg_type + package_id + message_size + body)
+
+                for message in self.messages_to_sent:
+                    self.con.write(message)
+                    self.messages_sent.append(message)
+                self.messages_to_sent = []
+
+        except Exception as e:
+            logging.error("run failed with exception:", e)
+            logging.error(e)
+            logging.error("previously received messages:")
+            for message in self.messages_received:
+                logging.error(message)
+            raise e
---
+++ test_protocol.py
@@ -0,0 +1,226 @@
+import logging
+import unittest
+
+import math
+from parameterized import parameterized
+
+import util
+import serial
+
+from panto_protocol import PantoProtocol, PositionData
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class StrictPantoProtocol(PantoProtocol):
+    def verify_position(self, position: PositionData):
+        if position.g_x1 != 0:
+            raise AssertionError('Invalid position data')
+        if position.g_y1 != 0:
+            raise AssertionError('Invalid position data')
+        if position.g_x2 != 0:
+            raise AssertionError('Invalid position data')
+        if position.g_y2 != 0:
+            raise AssertionError('Invalid position data')
+
+
+class ReadEncoderProtocol(PantoProtocol):
+
+    def __init__(self, con, timeout):
+        super(ReadEncoderProtocol, self).__init__(con, timeout)
+        self.positions = []
+
+    def verify_position(self, position: PositionData):
+        logging.info(f"{position.x1}, {position.y1}")
+        self.positions.append(position)
+
+    """
+    returns the orientation of the handle in radians [0 - 2pi] 
+    """
+
+    def panto_to_unity_rotation(self, rotation: int) -> float:
+        return rotation % 360 / (180 / math.pi)
+
+    def panto_to_unity_position_x(self, pos: int):
+        return pos / 10
+
+    def panto_to_unity_position_z(self, pos: int):
+        return pos / 10
+
+    def panto_to_unity_position(self, position: PositionData):
+        if position is None:
+            return None
+        return PositionData(
+            self.panto_to_unity_position_x(position.x1),
+            self.panto_to_unity_position_z(position.y1),
+            self.panto_to_unity_position_x(position.x2),
+            self.panto_to_unity_position_z(position.y2),
+            self.panto_to_unity_rotation(position.r1),
+            self.panto_to_unity_rotation(position.r2),
+            self.panto_to_unity_position_x(position.x1),
+            self.panto_to_unity_position_z(position.y1),
+            self.panto_to_unity_position_x(position.x2),
+            self.panto_to_unity_position_z(position.y2),
+        )
+
+
+class GodobjectMovingProtocol(PantoProtocol):
+    def __init__(self, port, timeout):
+        super(GodobjectMovingProtocol, self).__init__(port, timeout)
+        self.positions = [
+            (0, 0),
+            (10, 0),
+            (10, 10),
+            (0, 10)
+        ]
+        self.position_index = 0
+
+    def verify_position(self, position: PositionData):
+        pos = self.positions[self.position_index]
+        if position.g_x1 != pos[0] or position.g_y1 != pos[1]:
+            logging.warning(
+                f'Invalid god position, expected {pos[0]} {pos[1]} received {position.g_x1} {position.g_y1}')
+
+    def before_message(self):
+        if len(self.messages_received) > 100 and len(self.messages_received) % 25 == 0:
+            self.position_index += 1
+            self.position_index %= len(self.positions)
+            position = self.positions[self.position_index]
+            x_g = position[0]
+            y_g = position[1]
+            rotation = 0
+
+            x_bytes = x_g.to_bytes(4, byteorder='big')
+            y_bytes = y_g.to_bytes(4, byteorder='big')
+            r_bytes = rotation.to_bytes(4, byteorder='big')
+
+            self.messages_to_sent.append(bytes.fromhex("44 50 90 00 00 0E 00 01") + x_bytes + y_bytes + r_bytes)
+
+
+class ObstacleProtocol(PantoProtocol):
+    def __init__(self, port, timeout):
+        super(ObstacleProtocol, self).__init__(port, timeout)
+        self.num_obstacles = 1
+
+    def before_message(self):
+        if len(self.messages_received) % 25 == 0 and 1 < len(self.messages_received) < 25 * 100:
+            x1_bytes = (0).to_bytes(4, byteorder='big')
+            y1_bytes = (0).to_bytes(4, byteorder='big')
+            x2_bytes = (10).to_bytes(4, byteorder='big')
+            y2_bytes = (10).to_bytes(4, byteorder='big')
+
+            pkg_index = self.num_obstacles.to_bytes(1, byteorder='big')
+            obstacle_id_bytes = self.num_obstacles.to_bytes(2, byteorder='big')
+
+            self.num_obstacles += 1
+
+            length_bytes = (1 + 2 + 4 * 4).to_bytes(2, byteorder='big')
+
+            panto_index = (255).to_bytes(1, byteorder='big')
+
+            self.messages_to_sent.append(bytes.fromhex("44 50 A0") + pkg_index + length_bytes + panto_index
+                                         + obstacle_id_bytes + x1_bytes + y1_bytes + x2_bytes + y2_bytes)
+
+
+class BaseProtocolTest(unittest.TestCase):
+    con = None
+
+    def setUp(self):
+        util.upload_firmware("firmware/10 panto firmware/firmware", False)
+        com = "/dev/cu.usbserial-0001"
+        self.con = serial.Serial(com, baudrate=115200, timeout=5)
+
+    def tearDown(self):
+        self.con.close()
+
+
+class TestPantoProtocol(BaseProtocolTest):
+    def test_timeout_1min(self):
+        pp = StrictPantoProtocol(self.con, timeout=60)
+        pp.run()
+
+    def test_message_count(self):
+        pp = StrictPantoProtocol(self.con, timeout=60)
+        pp.run()
+        self.assertTrue(len(pp.messages_received) > 100)
+
+    def test_set_god_object(self):
+        pp = GodobjectMovingProtocol(self.con, timeout=60)
+        pp.run()
+        self.assertTrue(len(pp.messages_received) > 100)
+
+    def test_set_obstacle(self):
+        pp = ObstacleProtocol(self.con, timeout=60)
+        pp.run()
+        self.assertTrue(len(pp.messages_received) > 100)
+        self.assertTrue(pp.num_obstacles > 5)
+
+
+class MovePantoManuallyTest(BaseProtocolTest):
+    @parameterized.expand(
+        [
+            [
+                "upper pantograph move y",
+                "move",
+                "upper pantograph",
+                lambda x: x.y1,
+                "1cm in Y direction",
+                10
+            ],
+            [
+                "upper pantograph move x",
+                "move",
+                "upper pantograph",
+                lambda x: x.x1,
+                "1cm in X direction",
+                10
+            ],
+            [
+                "lower pantograph move y",
+                "move",
+                "lower pantograph",
+                lambda x: x.y2,
+                "1cm in Y direction",
+                10
+            ],
+            [
+                "lower pantograph move x",
+                "move",
+                "lower pantograph",
+                lambda x: x.x2,
+                "1cm in X direction",
+                10
+            ],
+            [
+                "upper handle rotation",
+                "rotate",
+                "upper handle",
+                lambda x: x.r1,
+                "15 degrees",
+                10 * math.pi / 180
+            ],
+            [
+                "lower handle rotation",
+                "rotate",
+                "lower handle",
+                lambda x: x.r2,
+                "15 degrees",
+                10 * math.pi / 180
+            ]
+        ]
+    )
+    def test_read_encoder(self, name, verb, dimension, extraction_function, move_thresh_str, move_thresh):
+        print("---- this test needs manual input -----")
+        input(f"please press enter and THEN {verb} the {dimension} by more than {move_thresh_str}")
+        pp = ReadEncoderProtocol(self.con, timeout=1)
+        pp.run()
+        pos_start = extraction_function(pp.panto_to_unity_position(pp.positions[0]))
+        pos_end = extraction_function(pp.panto_to_unity_position(pp.positions[-1]))
+        logging.info(f"starting: {pos_start}")
+        logging.info(f"ending: {pos_end}")
+        print(f"registered a move of {abs(pos_start - pos_end)}")
+        self.assertTrue(abs(pos_start - pos_end) > move_thresh)
+
+    def test_read_encoder_live(self):
+        pp = ReadEncoderProtocol(self.con, timeout=60)
+        pp.run()
