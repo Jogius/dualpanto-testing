58d548ea676b6b63bb829ee5661eb31ad217b27a
Julian Lehmann <julian@lehmann-wildau.de>
2024-04-15 23:56:38 +0200

    working

--- firmware/04 encoder read/src/main.cpp
+++ firmware/04 encoder read/src/main.cpp
@@ -6,26 +6,28 @@ static const uint32_t c_hspiSsPin1 = 15;
 static const uint32_t c_hspiSsPin2 = 5;
 static const uint16_t c_nop = 0x0;
 static const uint16_t c_clearError = 0x4001;
-static const uint16_t c_readAngle = 0xFFFF;
+static const uint16_t c_readAngle = 0xFFFF; //0xFFFF
 static const uint16_t c_dataMask = 0x3FFF;
 std::vector<uint16_t> m_values;
 
 int incomingByte = 0;    // for incoming serial data
-SPIClass m_spi;
-SPISettings m_settings;
+SPIClass m_spi(HSPI);
+SPISettings m_settings(10000000, SPI_MSBFIRST, SPI_MODE1);
 
 int channel = 0;
+int c_i = 0;
+uint16_t buf = 0;
 
 void setup()
 {
     Serial.begin(9600);    // opens serial port, sets data rate to 9600 bps
-    SPI.begin ();
-    SPI.setClockDivider(SPI_CLOCK_DIV128);
 
     pinMode(13, OUTPUT);
     pinMode(c_hspiSsPin1, OUTPUT);
     pinMode(c_hspiSsPin2, OUTPUT);
     m_values.resize(4, 0);
+
+    m_spi.begin();
 }
 
 void loop(){
@@ -36,10 +38,13 @@ void loop(){
   //   // say what you got:
   //   Serial.write(incomingByte);
   // }
-
-    uint16_t buf = 0;
+    
+    
     m_spi.beginTransaction(m_settings);
     digitalWrite(13, HIGH);
+    digitalWrite(c_hspiSsPin1, HIGH);
+    digitalWrite(c_hspiSsPin2, HIGH);
+    
 
     if (channel == 0) {digitalWrite(c_hspiSsPin1, LOW);}
     else if(channel == 1) {digitalWrite(c_hspiSsPin2, LOW);}
@@ -69,6 +74,8 @@ void loop(){
     digitalWrite(c_hspiSsPin2, HIGH);
 
     m_spi.endTransaction();
+    c_i++;
+    channel = c_i % 2;
 
     // for(int i=0; i < 4; i++){
     //     m_encoders[i].m_lastValidAngle = m_values[i];
