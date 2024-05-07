edf8b5f1edbc0e6aea015e741b8bcd3537a5414b
Julian Lehmann <julian@lehmann-wildau.de>
2024-05-07 16:52:38 +0200

    zero encoders

--- firmware/05 move handles in sync/src/main.cpp
+++ firmware/05 move handles in sync/src/main.cpp
@@ -40,8 +40,8 @@ static const uint16_t endeffectorPWMPin[2] = {25, 23};
 // #####
 
 //int incomingByte = 0;    // for incoming serial data
-int16_t encoders[6];
-int16_t encoder_zero[6];
+int32_t encoders[6] = {0,0,0,0,0,0};
+int32_t encoder_zero[6];
 
 void setup_encoders(){
   pinMode(13, OUTPUT);
@@ -60,8 +60,9 @@ void setup_encoders(){
   m_spi.begin();
 }
 
-int16_t overflow_correction[6] = {0,0,0,0,0,0};
-int16_t new_encoder_pos[6];
+int32_t overflow_correction[6] = {0,0,0,0,0,0};
+uint16_t new_encoder_pos[6];
+uint16_t last_encoder_pos[6] = {0,0,0,0,0,0};
 void loop_encoders(){
   for (int channel = 0; channel < 2; channel++){
     m_spi.beginTransaction(m_settings);
@@ -109,18 +110,111 @@ void loop_encoders(){
   }
 
   for (int i = 0; i < 6; i++){
+    if (i == 1 || i == 2) { new_encoder_pos[i] = - new_encoder_pos[i];}
+
     if (i < 4){
-      if (encoders[i] - overflow_correction[i] - new_encoder_pos[i] > 10000){
-        overflow_correction[i] += 16383;
-      }
-      if (encoders[i] - overflow_correction[i] - new_encoder_pos[i] < -10000){
-        overflow_correction[i] -= 16383;
-      }
-      encoders[i] = new_encoder_pos[i] + overflow_correction[i];
+    int32_t diff = new_encoder_pos[i] - last_encoder_pos[i];
+    if (abs(new_encoder_pos[i] - last_encoder_pos[i] - 16383) < abs(diff)){
+      //diff -= 16383;
+      overflow_correction[i] -= 16383;
+    } else if (abs(new_encoder_pos[i] - last_encoder_pos[i] + 16383) < abs(diff)){
+      //diff += 16383;
+      overflow_correction[i] += 16383;
+    }
+    //encoders[i] += diff;
+      // if (encoders[i] - overflow_correction[i] - new_encoder_pos[i] > 10000){
+      //   overflow_correction[i] += 16383;
+      // }
+      // if (encoders[i] - overflow_correction[i] - new_encoder_pos[i] < -10000){
+      //   overflow_correction[i] -= 16383;
+      // }
+      encoders[i] = new_encoder_pos[i] + overflow_correction[i] - encoder_zero[i];
     } else {
       encoders[i] = abs(new_encoder_pos[i] % (136 * 2));
     }
+    last_encoder_pos[i] = new_encoder_pos[i];
   }
+}
+
+bool calcParity(bool flag, uint16_t data)
+{
+    uint16_t temp = (data & c_dataMask) | (flag << 14);
+    // calculate https://en.wikipedia.org/wiki/Hamming_weight
+    temp -= (temp >> 1) & 0b0101010101010101;
+    temp = (temp & 0b0011001100110011) + ((temp >> 2) & 0b0011001100110011);
+    temp = (temp + (temp >> 4)) & 0b0000111100001111;
+    return ((temp * 0b0000000100000001) >> 8) % 2;
+}
+
+uint16_t msg(bool flag, uint16_t data){
+  
+}
+
+void zero_encoders(std::vector<uint16_t> newZero){
+{
+  m_spi.transfer16(0x16 | 0 | calcParity(0, 0x16)); //c_highZeroWrite
+
+	m_spi.beginTransaction(m_settings);
+	digitalWrite(c_hspiSsPin1, LOW);
+    for(auto i = 0; i < 2; ++i)
+    {
+      m_spi.transfer16(newZero >> 6);
+      m_encoders[i].transfer(SPIPacket(0, newZero[i] >> 6).m_transmission);
+    }
+	digitalWrite(c_hspiSsPin1, HIGH);
+	// m_spi.endTransaction();
+
+    //lower handle
+	// m_spi.beginTransaction(m_settings);
+	digitalWrite(c_hspiSsPin2, LOW);
+	for (auto i = 0; i < m_numberOfEncoders / 2; ++i)
+	{
+		m_encoders[i+2].transfer(SPIPacket(0, newZero[i+2] >> 6).m_transmission);
+	}
+	digitalWrite(c_hspiSsPin2, HIGH);
+	m_spi.endTransaction();
+
+    transfer(SPICommands::c_lowZeroWrite);
+
+    // begin();
+    // for(auto i = 0; i < m_numberOfEncoders; ++i)
+    // {
+    //     m_encoders[i].transfer(SPIPacket(0, newZero[i] & 0b111111).m_transmission);
+    // }
+    // end();
+
+    m_spi.beginTransaction(m_settings);
+	digitalWrite(c_hspiSsPin1, LOW);
+    for(auto i = 0; i < m_numberOfEncoders/2; ++i)
+    {
+        m_encoders[i].transfer(SPIPacket(0, newZero[i] & 0b111111).m_transmission);
+    }
+	digitalWrite(c_hspiSsPin1, HIGH);
+	// m_spi.endTransaction();
+
+    //lower handle
+	// m_spi.beginTransaction(m_settings);
+	digitalWrite(c_hspiSsPin2, LOW);
+	for (auto i = 0; i < m_numberOfEncoders / 2; ++i)
+	{
+		m_encoders[i+2].transfer(SPIPacket(0, newZero[i+2] & 0b111111).m_transmission);
+	}
+	digitalWrite(c_hspiSsPin2, HIGH);
+	m_spi.endTransaction();
+
+    //transfer(SPICommands::c_readAngle);
+}
+}
+
+void send_encoders(){
+  for (int i = 0; i < 6; i++){
+    Serial.printf("%d,", encoders[i] - encoder_zero[i]);
+  }
+  Serial.print(" -- ");
+  for (int i = 0; i < 6; i++){
+    Serial.printf("%d,", encoders[i]);
+  }
+  Serial.println("");
 }
 
 void setup_motors(){
@@ -162,15 +256,57 @@ void reset_motors(){
 
 void move_to_end(uint16_t pid){
   loop_encoders();
-  int last_encoder_pos = encoders[pid % 4] - 4;
-  while ((abs(last_encoder_pos - encoders[pid % 4]) > 3)){ 
-    int new_speed = (100 + abs(last_encoder_pos - encoders[pid % 4])) / 100;
-    ledcWrite(pid, (1 / new_speed) * 0.2*PWM_MAX);
+  int last_pos = encoders[pid % 4] - 4;
+  while ((abs(last_pos - encoders[pid % 4]) > 3)){ 
+    int new_speed = (100 + abs(last_pos - encoders[pid % 4])) / 100;
+    ledcWrite(pid, (1 / new_speed) * 0.3*PWM_MAX);
     delay(10);
     ledcWrite(pid, 0);
     //delay(10);
-    last_encoder_pos = encoders[pid % 4];
+    last_pos = encoders[pid % 4];
     loop_encoders();
+  }
+}
+
+void move_to(int32_t pos[6]){
+  reset_motors();
+  loop_encoders();
+  int32_t last_encoders[6];
+  for (int i = 0; i < 6; i++){last_encoders[i] = encoders[i];}
+  int moving = 4;
+  while (moving > 0){
+    moving = 4;
+
+    for (int i = 0; i < 4; i++){
+      // forwards
+      if (pos[i] - encoders[i] > 100){
+        ledcWrite(i+4, 0);
+        //Serial.printf("%d forward", i);
+        int new_speed = (1 + abs(pos[i] - encoders[i])) / 1;
+        ledcWrite(i, 0.1*PWM_MAX - ((1 / new_speed) * 0.1*PWM_MAX));
+        
+        //ledcWrite(i, 0.1*PWM_MAX);
+      } else if (pos[i] - encoders[i] < -100){
+        ledcWrite(i, 0);
+        //Serial.printf("%d bacjward", i);
+        int new_speed = (1 + abs(pos[i] - encoders[i])) / 1;
+        ledcWrite(i + 4, 0.1*PWM_MAX - ((1 / new_speed) * 0.1*PWM_MAX));
+        
+        //ledcWrite(i + 4, 0.1*PWM_MAX);
+      }
+      else{
+        //Serial.printf("%d none", i);
+        ledcWrite(i, 0);
+        ledcWrite(i + 4, 0);
+        moving -= 1;
+      }
+      loop_encoders();
+    }
+    delay(1);
+
+    for (int i = 0; i < 6; i++){last_encoders[i] = encoders[i];}
+    loop_encoders();
+    send_encoders();
   }
 }
 
@@ -179,88 +315,29 @@ void align_motors(){
   // find zero position
   // move shorter handle to max
   loop_encoders();
-  int last_encoder_pos0 = encoders[0] -100;
-  int last_encoder_pos1 = encoders[1] - 100;
 
-  // for (int i = 0; i < 100; i++){
-  //   ledcWrite(4, 0.2*PWM_MAX);
-  //   delay(5);
-  //   ledcWrite(4, 0);
-  //   delay(25);
-  // }
+  // upper handle
   move_to_end(5);
   ledcWrite(5, 0.4*PWM_MAX);
   move_to_end(4);
   ledcWrite(5, 0);
-
+  for (int i = 0; i < 2; i++){ encoder_zero[i] = encoders[i];}
+  //lower handle
   move_to_end(6);
   ledcWrite(6, 0.4*PWM_MAX);
   move_to_end(7);
   ledcWrite(6, 0);
-
-  // for (int i = 0; i < 10; i++){
-  //   ledcWrite(7, 0.4*PWM_MAX);
-  //   delay(5);
-  //   ledcWrite(7, 0);
-  //   delay(25);
-  // }
-  
-  
-  // while ((abs(last_encoder_pos0 - encoders[0]) > 3)){
-  //   last_encoder_pos0 = encoders[0];
-  //   ledcWrite(5, 0.4*PWM_MAX);
-  //   //ledcWrite(4, 0.2*PWM_MAX);
-  //   delay(5);
-  //   ledcWrite(5, 0);
-  //   //ledcWrite(4, 0);
-  //   delay(25);
-  //   loop_encoders();
-  // }
-
-  for (int i = 0; i < 2; i++){ encoder_zero[i] = encoders[i];}
-
-  delay(2000);
-
-  // lower handle
-
-  int last_encoder_pos2 = encoders[2] -100;
-  int last_encoder_pos3 = encoders[3] - 100;
-
-  
-
-  // while ((abs(last_encoder_pos2 - encoders[2]) > 3)){ //|| (abs(last_encoder_pos2 - encoders[2]) > 30)){
-
-  //   last_encoder_pos2 = encoders[2];
-    
-  //   ledcWrite(6, 0.4*PWM_MAX);
-  //   //ledcWrite(3, 0.3*PWM_MAX);
-  //   delay(5);
-  //   ledcWrite(6, 0);
-  //   //ledcWrite(3, 0);
-  //   delay(25);
-  //   loop_encoders();
-  // }
-
-
-  // while ((abs(last_encoder_pos3 - encoders[3]) > 3)){
-  //   last_encoder_pos3 = encoders[3];
-  //   ledcWrite(6, 0.2*PWM_MAX);
-  //   ledcWrite(7, 0.2*PWM_MAX);
-  //   delay(10);
-  //   ledcWrite(6, 0);
-  //   ledcWrite(7, 0);
-  //   delay(100);
-  //   loop_encoders();
-  // }
   for (int i = 2; i < 4; i++){ encoder_zero[i] = encoders[i];}
+  int correction[4] = {3700, -4000, -4000, 3700};
+  for (int i = 0; i < 4; i++){ encoder_zero[i] -= correction[i];}
 
-  while ((encoders[0] - encoder_zero[0]) > -3700){
-    ledcWrite(4, 0.4*PWM_MAX);
-    delay(5);
-    ledcWrite(4, 0);
-    delay(25);
-    loop_encoders();
-  }
+  // while ((encoders[0] - encoder_zero[0]) > -3700){
+  //   ledcWrite(4, 0.4*PWM_MAX);
+  //   delay(5);
+  //   ledcWrite(4, 0);
+  //   delay(25);
+  //   loop_encoders();
+  // }
 
   // while ((encoders[2] - encoder_zero[2]) > -3700){
   //   ledcWrite(7, 0.4*PWM_MAX);
@@ -272,24 +349,30 @@ void align_motors(){
   //for (int i = 0; i < 4; i++){ encoder_zero[i] = encoders[i];}
 }
 
+void loop_motors(){
+  
+}
+
 void setup(){
   Serial.begin(9600);    // opens serial port, sets data rate to 9600 bps
   setup_encoders();
   setup_motors();
   reset_motors();
   delay(100);
-  align_motors();
+  //align_motors();
+  
+  loop_encoders();
+  zero_encoders();
+  loop_encoders();
+  int32_t new_encoders[6];
+  for (int i = 0; i < 6; i++){new_encoders[i] = encoders[i];}
+  new_encoders[0] += 1000;
+  move_to(new_encoders);
 }
 
 void loop(){  
+  delay(10);
   //loop_motors();
   loop_encoders();
-  for (int i = 0; i < 6; i++){
-    Serial.printf("%d,", encoders[i] - encoder_zero[i]);
-  }
-  Serial.print(" -- ");
-  for (int i = 0; i < 6; i++){
-    Serial.printf("%d,", encoders[i]);
-  }
-  Serial.println("");
+  send_encoders();
 }
